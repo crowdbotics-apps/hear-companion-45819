eb739b90ccba4b90735851aff43c19f28b28d62c
Crowdbotics <team@crowdbotics.com>
2024-02-03 22:36:07 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hear-companion-45819",
+  url: "https://hear-companion-45819.botics.co",
+  api: "https://hear-companion-45819.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
